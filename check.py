0f8bb3fb6d75e0659dea33a5f6854f48560aff75
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2022-03-02 17:43:08 +0300

    Add files via upload

--- check.py
+++ check.py
@@ -2,31 +2,32 @@ import string
 
 
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-
+alphabet_digits = len(str(len(alphabet)))
 
 def encode(text: str) -> str:
     result = ""
     for i in text:
-        idx = 
-        alphabet_digits = len(str(len(alphabet)))
-        if len(str(alphabet_digits)) == alphabet_digits:
-            result += str(alphabet_digits)
+        if i not in alphabet:
+            return None
+        idx = alphabet.index(i)
+        if len(str(idx)) == alphabet_digits:
+            result += str(idx)
         else:
             zeroes = alphabet_digits - len(str(idx))
             for j in range(zeroes):
                     result += "0"
-                result += str(alphabet_digits)
+                result += str(idx)
     return result
             
 def decode(text: str) -> str:
-    slices = len(text) // 3
+    slices = len(text) // alphabet_digits
     iterator = 0
     for i in range(slices):
-        symbol = text[iterator:iterator + 3]
+        symbol = text[iterator:iterator + alphabet_digits]
         if symbol == "000"
             result += alphabet[0]
         else:
             idx = symbol.lstrip("0")
             result += alphabet.index[int(idx)]
-        iterator += 3
+        iterator += alphabet_digits
     return result
